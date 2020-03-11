53b576a101267bfd5923e73638c4cd44f477955f
ALIENWARE-CHARL\Charles <CharlesFeng207@gmail.com>
2020-03-11 21:23:00 +0800

    1

--- AndroidBuild_FC.py
+++ AndroidBuild_FC.py
@@ -35,8 +35,8 @@ tasks.append(unity_tools.execute_update_asset_config)
 
 tasks.append(lambda : unity_tools.execute_unity("wxb.GenHotfixDegleng.OnekeyGenFinal"))
 
-tasks.append(unity_tools.execute_update_assetbundle)
-# tasks.append(unity_tools.execute_fully_update_assetbundle)
+# tasks.append(unity_tools.execute_update_assetbundle)
+tasks.append(unity_tools.execute_fully_update_assetbundle)
 
 # tasks.append(unity_tools.commitAssetBundles)
 tasks.append(lambda : unity_tools.execute_quick_build("Firebase.asset"))
@@ -46,6 +46,6 @@ tasks.append(unity_tools.execute_upload_lastbuild)
 # tasks.append(unity_tools.execute_archieve_assetbundle)
 
 tasks.append(unity_tools.execute_upload_pending_version)
-# tasks.append(unity_tools.execute_upload_version)
+tasks.append(unity_tools.execute_upload_version)
 
 unity_tools.do_tasks(tasks)
